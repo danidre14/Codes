107ddae0ee9070c36f3d284d02ee4e7860a0b8d6
danidre14 <danidre14@gmail.com>
2018-10-09 08:11:28 -0400

    Update APCodes.js

--- APCodes.js
+++ APCodes.js
@@ -1,1 +1,10 @@
-testit();
+const loadAnim = function() {
+  var ap1to27 = "";
+  var ap28to50 = "";
+  var ap51to65 = "";
+  var ap66to67 = "";
+  var ap68to80 = "";
+  var a = ap1to27, b = ap28to50, c = ap51to65, d = ap66to67, e = ap68to80;
+  var code = a + b + c + d + e;
+  loadCode(code);
+}
